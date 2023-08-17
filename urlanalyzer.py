b5084a2ccc667e8e7515381fac9f19cc4ee016cb
Dinesh Katam <39465454+dino1729@users.noreply.github.com>
2023-08-16 18:47:06 -0700

    updated context window in urlanalyzer

--- urlanalyzer.py
+++ urlanalyzer.py
@@ -220,17 +220,21 @@ if __name__ == "__main__":
         LLM_DEPLOYMENT_NAME = "text-davinci-003"
         LLM_MODEL_NAME = "text-davinci-003"
         openai.api_version = os.environ.get("AZURE_API_VERSION")
+        max_input_size = 4096
+        context_window = 4096
+        print("Using text-davinci-003 model.")
     else:
         LLM_DEPLOYMENT_NAME = "gpt-3p5-turbo-16k"
         LLM_MODEL_NAME = "gpt-35-turbo-16k"
         openai.api_version = os.environ.get("AZURE_CHATAPI_VERSION")
+        max_input_size = 16384
+        context_window = 16384
+        print("Using gpt-3p5-turbo-16k model.")
 
     # max LLM token input size
-    max_input_size = 4096
     num_output = 1024
     max_chunk_overlap_ratio = 0.1
     chunk_size = 512
-    context_window = 4096
     prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap_ratio)
     text_splitter = SentenceSplitter(
     separator=" ",
