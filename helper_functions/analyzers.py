6e5ebbc4040f4a236767a21e3e221252e84dfda4
Dinesh Katam <39465454+dino1729@users.noreply.github.com>
2024-04-29 22:11:18 -0700

    Update node parser and text splitter

--- helper_functions/analyzers.py
+++ helper_functions/analyzers.py
@@ -23,7 +23,7 @@ from llama_index.llms.azure_openai import AzureOpenAI
 from llama_index.core import VectorStoreIndex, PromptHelper, SimpleDirectoryReader, StorageContext, load_index_from_storage, get_response_synthesizer
 from llama_index.core.indices import SummaryIndex
 from llama_index.core.query_engine import RetrieverQueryEngine
-from llama_index.core.node_parser import SemanticSplitterNodeParser
+from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core import PromptTemplate
 from llama_index.core import Settings
 
@@ -79,7 +79,8 @@ Settings.embed_model = AzureOpenAIEmbedding(
     max_retries=3,
     embed_batch_size=1,
 )
-Settings.splitter = SemanticSplitterNodeParser(buffer_size=1, breakpoint_percentile_threshold=95, embed_model=Settings.embed_model)
+text_splitter = SentenceSplitter()
+Settings.text_splitter = text_splitter
 Settings.prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap_ratio)
 
 example_qs = []
